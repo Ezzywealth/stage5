d30cc21a8b814f6d94d7e05b74089387e5f7a1af
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-09-29 13:26:45 +0100

    COmplete video show page

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,6 +1,25 @@
 import React, { useState } from 'react';
 import AuthNav from '../../components/AuthNav';
 import Image from 'next/image';
+import VideoList from '../../components/Video/Videos';
+const videos = [
+	{
+		videoUrl: 'https://example.com/video1.mp4',
+		title: 'How to create Facebook Ad listing',
+		date: 'SEPTEMBER 23, 2023',
+	},
+	{
+		videoUrl: 'https://example.com/video2.mp4',
+		title: 'How to create Facebook Ad listing',
+		date: 'SEPTEMBER 23, 2023',
+	},
+	{
+		videoUrl: 'https://example.com/video3.mp4',
+		title: 'How to create Facebook Ad listing',
+		date: 'SEPTEMBER 23, 2023',
+	},
+	// Add more video objects as needed
+];
 
 const Home = () => {
 	const [searchQuery, setSearchQuery] = useState('');
@@ -10,8 +29,8 @@ const Home = () => {
 	return (
 		<>
 			<AuthNav />
-			<main className=' w-full divide-y-2 divide-[rgba(0,0,0,0.1)]'>
-				<section className='flex w-full items-center justify-between border-b-2 py-12 border-[rgba(0,0,0,0.1)]  p-4 px-4 lg:px-16 xl:px-36 '>
+			<main className=' w-full '>
+				<section className='flex w-full items-center justify-between border-b-2 py-4 border-[rgba(0,0,0,0.1)]  p-4 px-4 lg:px-16 xl:px-36 '>
 					<div>
 						<h1 className='text-[32px] font-bold text-[#141414]'>Hello, John Mark</h1>
 						<p className='text-[#141414]/70 text-lg font-normal'>Here are your recorded videos</p>
@@ -22,6 +41,9 @@ const Home = () => {
 							<input type='text' className='w-full placeholder:text-sm focus:outline-none bg-transparent' value={searchQuery} onChange={(e) => setSearchQuery(e?.target?.value)} placeholder='Search for a particular video' />
 						</form>
 					</div>
+				</section>
+				<section className='p-4 px-4 lg:px-16 xl:px-36 '>
+					<VideoList videos={videos} />
 				</section>
 			</main>
 		</>
